e857fbdc9c4b62bcab2b460871e86ddc6bcdef28
gossoudarev <igossoudarev@herzen.spb.ru>
2016-04-14 23:08:21 +0300

    Single quotes

--- raw-chat/index.js
+++ raw-chat/index.js
@@ -2,44 +2,44 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var PORT = 5555;
-var chat = "chat.txt";
+var chat = 'chat.txt';
 
 var qry = function(requrl){
  return url.parse(requrl, true).query.what;
 };
 
 var serv = function (req, res){
-  console.log("Request!");
+  console.log('Request!');
   switch (req.url.substring(1,4)) {
-   case "add" :
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write("<h2>Добавляем "+ what + "</h2>");
+   case 'add' :
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h2>Добавляем '+ what + '</h2>');
         var what = qry(req.url);
         if (what) {
-          fs.appendFile(chat, "\n"+what, function(err){
+          fs.appendFile(chat, '\n'+what, function(err){
             if (err) throw err;
-            res.write("<h2><i>Добавлено</i> "+ what + "</h2>");
+            res.write('<h2><i>Добавлено</i> '+ what + '</h2>');
             res.end();
           });
         } else {
-          res.write("<h1><i>Нечего добавлять</i></h1>");
+          res.write('<h1><i>Нечего добавлять</i></h1>');
           res.end();         
         }
         
         break;
-   case "get" :
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write("<h1>Читаем...</h1>");
+   case 'get' :
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>Читаем...</h1>');
         fs.readFile(chat, function(err, what){
           if (err) throw err;
-          res.write("<pre>"+ what + "</pre>");
+          res.write('<pre>'+ what + '</pre>');
           res.end();
         });
         
         break; 
    default:
-        res.writeHead(200, {"Content-Type": "text/html"});
-        fs.readFile("./public/form.html", function(err, what){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile('./public/form.html', function(err, what){
           if (err) throw err;
           res.write(what);
           res.end();
@@ -47,4 +47,6 @@ var serv = function (req, res){
   }
 };
 
-http.createServer(serv).listen(PORT, function(){console.log(PORT)});
+http.createServer(serv).listen(PORT, function(){
+	console.log(PORT)
+});
