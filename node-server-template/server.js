6a73411eca0d0a824e3ed9b627a4bde0a9c9dc0b
gossoudarev <igossoudarev@herzen.spb.ru>
2016-05-01 18:46:38 +0300

    update node server template es2015

--- node-server-template/server.js
+++ node-server-template/server.js
@@ -1,35 +1,32 @@
-var PORT = 4444;
-var http = require('http'),
-    fs = require('fs');
+const PORT = 4444;
+var   http = require('http'),
+      fs = require('fs');
      
-
-module.exports = (function(){
+module.exports = (()=>{
    function inner(){
-      this.start = function (whatToDo){
-           	  
-		  http.createServer(function(req, res){
+      this.start = whatToDo=>{
+		  http.createServer((req, res)=>{
 		  	
 			  switch ( req.url.substring(0,6)  ) {
-			  case "/about" :
-			       res.writeHead(200, {"Content-Type": "text/html"});
+			  case '/about' :
+			       res.writeHead(200, {'Content-Type': 'text/html'});
 			       res.write( '<h1>Hello world!</h1>' );   
-
 			       res.end();
+				   whatToDo('about called!');
 			       break;
 
 			  default:
-			       res.writeHead(200, {"Content-Type": "text/html"});
-			       fs.readFile("public/page.html", function(err, what){
+			       res.writeHead(200, {'Content-Type': 'text/html'});
+			       fs.readFile('public/page.html', (err, what)=>{
 			          if (err) throw err;
 			          res.write(what);
 			          res.end();
 			       });
 			 }
 			
-			
-		  }).listen(process.env.port || PORT, function(){
-		  	  console.log(PORT)
-		  });
+		  }).listen(process.env.port||PORT,()=>
+		  	        console.log('--> Port %d listening!',PORT)
+	         );
       };   
     }
   return new inner;
