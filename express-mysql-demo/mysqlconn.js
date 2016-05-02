c6ebfcfb3e5a986e2322b63540078a53740288af
gossoudarev <igossoudarev@herzen.spb.ru>
2016-05-02 13:51:39 +0300

    express-mysql-demo

---
+++ express-mysql-demo/mysqlconn.js
@@ -0,0 +1,29 @@
+const BD = '/var/www/bd.json';
+var   mysql = require('mysql'),
+	  conn = mysql.createConnection( require(BD) );
+module.exports = (()=>{ 
+	function inner(){
+		this.querySelect =  (myQuery, whatToDo)=>{
+	    	var     resArr,
+		            key = 'jq',
+		  			myQuery1 = "SELECT `key` FROM  `html1` WHERE `key` LIKE  '%" + key + "%'";
+			myQuery = myQuery || myQuery1; //запрос по умолчанию
+
+			conn.query(myQuery, (err,rows)=>{
+				try {
+					if (err) throw err;
+					resArr = Object.keys(rows).map(i=>rows[i].key);	
+					whatToDo(null, resArr);	
+					//это вызов функции, к-рая будет передана в querySelect из server.js
+					//она должна иметь форм. парам., куда попадёт рез-т из БД - resArr	     
+				} 
+				catch (e) {
+					//это например если сервер БД отключён
+					//это точно работает, а conn.connect(...) - не работает
+					conn 	   = mysql.createConnection(  require(BD)   );
+					whatToDo(e, null);
+				}
+			}); 
+		}; 
+	}
+return new inner; })();
---
+++ express-mysql-demo/server.js
@@ -0,0 +1,43 @@
+const PORT = 5556;
+var	  express = require('express'),
+      app = express(),
+	  q = require('mysqlconn');
+     
+module.exports = (()=>{
+   function inner(){
+      this.start = whatToDo=>{
+		  app.use(express.static(__dirname + '/public'))
+		     .use((req, res, next)=>next()); //univers m/w
+   		  app.get('/', (req, res) => {
+   			    res.send('<h1>Welcome to Express!</h1>');
+   			    //res.sendFile(__dirname + '/index.html');  or res.redirect('/index.html');
+   		  }); 
+		  		  
+		  
+		  app.get('/search/:key', (req, res) => {
+			  	var key = req.params.key,	
+			        myQuery1 = "SELECT `key` FROM  `html1` WHERE `key` LIKE  '%" + key + "%'";
+			  	q.querySelect( myQuery1,   (err, x)=>{
+			  			//эта коллбэк-функция вызывается там, в недрах q
+			  			//когда готов результат из БД
+			  		if (err === null) {
+			  			res.json(x);		
+			  		} else {
+			  			res.json(err); 			
+			  		}
+			  	});			
+		  });		  
+		  
+		  
+   		  app.get('/api', (req, res) => {
+			  res.set({'Access-Control-Allow-Origin': '*', 'elias': 'goss'}); //CORS - outer reqs
+   			  res.json({'gossApi':'started ok!'});
+   		  });   
+		  app.listen(process.env.port||PORT,()=>console.log('--> Port %d listening!',PORT));
+      };   
+    }
+  return new inner;
+})();
+
+
+
