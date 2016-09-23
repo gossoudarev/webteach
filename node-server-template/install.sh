92103dca27976c78196d4d7091dd2932c1ce6112
gossoudarev <igossoudarev@herzen.spb.ru>
2016-09-23 19:53:25 +0300

    installation guide BETTER3

--- node-server-template/install.sh
+++ node-server-template/install.sh
@@ -3,6 +3,6 @@ cd node-server-template
 mkdir public
 wget https://raw.githubusercontent.com/gossoudarev/webteach/master/node-server-template/index.js
 wget https://raw.githubusercontent.com/gossoudarev/webteach/master/node-server-template/server.js
-wget https://raw.githubusercontent.com/gossoudarev/webteach/master/node-server-template/package.js
+wget https://raw.githubusercontent.com/gossoudarev/webteach/master/node-server-template/package.json
 cd public
 wget https://raw.githubusercontent.com/gossoudarev/webteach/master/node-server-template/public/page.html
