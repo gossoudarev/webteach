3145f27ed4201da48ef4de442e1543b3fdb2253c
gossoudarev <igossoudarev@herzen.spb.ru>
2016-09-23 19:27:12 +0300

    installation guide

---
+++ node-server-template/README.md
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/gossoudarev/webteach/master/node-server-template/package.json
+
+bash install.sh it will make a directory
---
+++ node-server-template/install.sh
@@ -0,0 +1,7 @@
+mkdir node-server-template
+cd node-server-template
+mkdir public
+wget https://raw.githubusercontent.com/gossoudarev/webteach/master/node-server-template/index.js
+wget https://raw.githubusercontent.com/gossoudarev/webteach/master/node-server-template/server.js
+wget https://raw.githubusercontent.com/gossoudarev/webteach/master/node-server-template/package.js
+wget https://raw.githubusercontent.com/gossoudarev/webteach/master/node-server-template/public/page.html
