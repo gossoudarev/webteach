e4521460509325686199836240529319e5424dc7
gossoudarev <igossoudarev@herzen.spb.ru>
2016-09-27 02:30:14 +0300

    simple_testing step 0b

--- simple_testing/install.sh
+++ simple_testing/install.sh
@@ -1,7 +1,8 @@
-mkdir simple-testing
-cd simple-testing
+mkdir simple_testing
+cd simple_testing
 
 wget https://raw.githubusercontent.com/gossoudarev/webteach/master/simple_testing/kramer_tests_set.json
 wget https://raw.githubusercontent.com/gossoudarev/webteach/master/simple_testing/testing55.js
 
-echo Now run phantomjs testing55.js kramer_tests_set.json
+echo Now cd simple_testing
+echo ...and  run phantomjs testing55.js kramer_tests_set.json
