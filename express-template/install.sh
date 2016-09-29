e79f25476cffc78cbe7a4b57e0abcf1f43acbbf4
gossoudarev <igossoudarev@herzen.spb.ru>
2016-09-30 02:33:33 +0300

    installer improved

--- express-template/install.sh
+++ express-template/install.sh
@@ -1,8 +1,11 @@
-mkdir node-server-template
-cd node-server-template
+mkdir express-template
+cd express-template
 mkdir public
 wget https://raw.githubusercontent.com/gossoudarev/webteach/master/express-template/index.js
 wget https://raw.githubusercontent.com/gossoudarev/webteach/master/express-template/server.js
 wget https://raw.githubusercontent.com/gossoudarev/webteach/master/express-template/package.json
 cd public
 wget https://raw.githubusercontent.com/gossoudarev/webteach/master/express-template/public/page.html
+
+echo cd express-temaplate
+echo npm start or PORT=XXXX npm start or port=YYYY pm2 start index.js --watch or...
