8b9e2b91cf71ee3f04f8ec34383a563f39286aae
gossoudarev <igossoudarev@herzen.spb.ru>
2016-10-11 04:51:00 +0300

    for teachers wildcards

---
+++ wildcards/install.bat
@@ -0,0 +1,16 @@
+mkdir files
+cd files
+echo.>0)ascii.docx
+echo.>1)basic.doc
+echo.>2)pascal.docx
+echo.>3)disca.doc
+echo.>4)musc.docx
+echo.>5)science.doc
+echo.>6)cplusic.docx
+echo.>7)screen.doc
+echo.>8)comic.docx
+echo.>9)tusc.doc
+
+echo 'run cd files'
+echo 'run dir ??s*.*'
+echo 'will give you 5)science.doc	7)screen.doc'
---
+++ wildcards/install.sh
@@ -0,0 +1,16 @@
+mkdir files
+cd files
+touch '0)ascii.docx'
+touch '1)basic.doc'
+touch '2)pascal.docx'
+touch '3)disca.doc'
+touch '4)musc.docx'
+touch '5)science.doc'
+touch '6)cplusic.docx'
+touch '7)screen.doc'
+touch '8)comic.docx'
+touch '9)tusc.doc'
+
+echo 'run cd files'
+echo 'run ls *us*c.*x'
+echo 'will give you 4)musc.docx        6)cplusic.docx'
