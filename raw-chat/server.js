9761c3218cf23e06e3302486ab1f62a329f31f8a
gossoudarev <igossoudarev@herzen.spb.ru>
2016-10-12 11:31:21 +0300

    hacked in public

--- raw-chat/server.js
+++ raw-chat/server.js
@@ -1,54 +1,52 @@
 /*jshint esversion: 6 */
 /*jshint -W058 */
 
-const PORT = 7654;
-var http = require('http'),
-    url = require('url'),
-    fs = require('fs'),
-    chat = 'chat.txt',
-    
-	qParse = requrl => url.parse(requrl, true).query;
+const PORT = 4444;
+var   http = require('http'),
+      fs = require('fs'),
+  	  url = require('url'),
+	  CHAT = 'chat.txt',
 
+	  qParse = requrl => url.parse(requrl, true).query;
+     
 module.exports = (()=>{
    function inner(){
       this.start = whatToDo=>{
 		  http.createServer((req, res)=>{
 		  	
-			  switch ( req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()  ) {
-			   case '/add' :
-			        res.writeHead(200, {'Content-Type': 'text/html'});
-			        res.write('<h2>Добавляем '+ what + '</h2>');
-			        var what = qParse(req.url).what;
-			        if (what) {
-			          fs.appendFile(chat, '\n'+what, function(err){
-			            if (err) throw err;
-			            res.write('<h2><i>Добавлено</i> '+ what + '</h2>');
-			            res.end();
-			          });
-			        } else {
-			          res.write('<h1><i>Нечего добавлять</i></h1>');
-			          res.end();         
-			        }
-        
-			        break;
-			   case '/get' :
-			        res.writeHead(200, {'Content-Type': 'text/html'});
-			        res.write('<h1>Читаем...</h1>');
-			        fs.readFile(chat, function(err, what){
-			          if (err) throw err;
-			          res.write('<pre>'+ what + '</pre>');
-			          res.end();
-			        });
-        
-			        break; 
-			   default:
-			        res.writeHead(200, {'Content-Type': 'text/html'});
-			        fs.readFile('./public/form.html', function(err, what){
-			          if (err) throw err;
-			          res.write(what);
-			          res.end();
-			        });
-			  }
+			  switch (  req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()  ) {
+				  case '/add' :
+				       res.writeHead(200, {'Content-Type': 'text/html'});
+					   var addedMessage = qParse(req.url).addedMessage;
+					   if (addedMessage) {
+				       		res.write( `<h2>Добавляем ${addedMessage}!</h2>` ); 
+					   	    fs.appendFile(CHAT, '\n'+addedMessage, err=>{
+					   	    	if (err) throw err;
+								res.end( `<h2>Добавлено ${addedMessage}!</h2>` ); 
+					   	    });
+					   } else {
+							res.end( `<h2>Нечего добавлять!</h2>` );
+					   }  
+				       
+					   //whatToDo('about called!');
+				       break;
+				  case '/read' : 	   
+					   res.writeHead(200, {'Content-Type': 'text/html'});
+					   res.write('<h2>Читаем...</h2>');
+					   fs.readFile(CHAT, (err, what)=>{
+					   	   if (err) throw err;
+						   res.end(`<pre>${what}</pre>`);
+					   });
+					   break;
+
+				  default:
+				       res.writeHead(200, {'Content-Type': 'text/html'});
+				       fs.readFile('public/form.html', (err, what)=>{
+				          if (err) throw err;
+				          res.write(what);
+				          res.end();
+				       });
+			 }
 			
 		  }).listen(process.env.PORT || PORT,()=>
 		  	        console.log(`--> Port ${  process.env.PORT || PORT  } listening!`)
