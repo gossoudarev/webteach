fdf86b587c75a8854aeb1cc3cdeee2b7774150a9
gossoudarev <igossoudarev@herzen.spb.ru>
2017-05-14 16:15:18 +0300

    closer to loop

--- testing_promise/mocha_import/test.js
+++ testing_promise/mocha_import/test.js
@@ -2,17 +2,24 @@
 import m from 'mongoose';
 import should from 'should';
 const      
-      UserSchema = new m.Schema({
+       UserSchema = new m.Schema({
                 	"user": { "type": "String" },
       		  		"password": { "type": "String" }
      			 }),
-       User = m.model('User', UserSchema);
+       User = m.model('User', UserSchema),
+	   testcases = [{name:'John', fname:'Resig'}, {name:'Dmitry', fname:'Ibragimov'}];
+       
 m.Promise = global.Promise;
 m.connect('mongodb://student_account:Qwerty.123@kodaktor.ru/experimental', {}, err=>{if(err)throw err;});
    
 describe('#findOne()', ()=> {
   it('respond with one matching record', ()=>
-     User.findOne({ name: 'John' }).should.eventually.have.property('_doc')
-         .with.property('fname','Resig')
+     User.findOne({ name: testcases[0].name }).should.eventually.have.property('_doc')
+         .with.property('fname',testcases[0].fname)
   );
+
+  it('respond with one matching record', ()=>
+       User.findOne({ name: testcases[1].name }).should.eventually.have.property('_doc')
+           .with.property('fname',testcases[1].fname)
+    );  
 });
