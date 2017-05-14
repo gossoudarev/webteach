7f0fd727c49c9f6225a8922ba8c37933af8da227
gossoudarev <igossoudarev@herzen.spb.ru>
2017-05-15 02:20:04 +0300

    rearrange testing dirs

--- simple_testing/install.sh
+++
@@ -1,8 +0,0 @@
-mkdir simple_testing
-cd simple_testing
-
-wget https://raw.githubusercontent.com/gossoudarev/webteach/master/simple_testing/kramer_tests_set.json
-wget https://raw.githubusercontent.com/gossoudarev/webteach/master/simple_testing/testing55.js
-
-echo Now cd simple_testing
-echo ...and  run phantomjs testing55.js kramer_tests_set.json
