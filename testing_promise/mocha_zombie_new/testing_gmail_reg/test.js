567e707bbf49f6d59a9748dd5aa3b2f60b1c8674
gossoudarev <igossoudarev@herzen.spb.ru>
2017-09-05 15:13:31 +0300

    updated

--- testing_promise/mocha_zombie_new/testing_gmail_reg/test.js
+++ testing_promise/mocha_zombie_new/testing_gmail_reg/test.js
@@ -1,14 +1,14 @@
 import Zombie from 'zombie';
 import should from 'should';
 import testValues from './testValues';
-((URL = 'https://kodaktor.ru/g/testing_f38ac?gmail=')=>{
+((URL = 'https://kodaktor.ru/g/testing_f38ac')=>{
     const page = new Zombie();
     const goURL = url=> new Promise(resolve=>page.visit(url, resolve));
-    const getResult = ()=>({response:page.document.querySelector('#response').textContent});
+    const getResult = ()=>({response:page.document.querySelector('h3#response').textContent});
 
     testValues.forEach (
 			o=>it('returns no or yes from URL', ()=>
-		  			goURL(URL+o.value).then(getResult).should.eventually.have
+		  			goURL(URL+'?test='+o.value).then(getResult).should.eventually.have
      	  		.property('response', o.expected)
 			))
 })();
--- testing_promise/mocha_zombie_new/testing_gmail_reg/urlcrawler.js
+++ testing_promise/mocha_zombie_new/testing_gmail_reg/urlcrawler.js
@@ -1,18 +1,18 @@
 import Zombie from 'zombie';
 import testValues from './testValues';
-export default function (URL = 'https://kodaktor.ru/g/testing_f38ac?gmail=') {
+export default function (URL = 'https://kodaktor.ru/g/testing_f38ac') {
 	const page = new Zombie();
 	const goURL = url=> new Promise(resolve=>page.visit(url, resolve));
 
 	//this is nice
 	(async()=>{
 		for (const o of testValues) {
-		    await goURL(URL + o.value);
+		    await goURL(URL + '?test=' + o.value);
 			try {
-	        	console.log(  `${URL + o.value}:  got  ${ page.document.querySelector('#response').textContent } expected ${o.expected} `   );
+	        	console.log(  `${URL + o.value}:  got  ${ page.document.querySelector('h3#response').textContent } expected ${o.expected} `   );
 	        } catch (e) {
 	        	console.log('Could NOT retrieve the wanted value!');
 	        }
-	    }    
+	    }
 	})();
 };
